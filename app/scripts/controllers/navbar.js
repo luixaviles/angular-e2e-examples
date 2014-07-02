85eb74b7bc9b3f9a450251620eb0760009c571c5
unknown <luis.aviles@ascbolaviles.bolivia.assuresoft.com>
2014-07-02 17:27:58 -0400

    first commit

---
+++ app/scripts/controllers/navbar.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularE2eExamplesApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+    
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
